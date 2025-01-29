3d788bf9e1425fc2da5aa050b765cf5b1a254640
Narakaaa <122264454+Narakaaa@users.noreply.github.com>
2025-01-29 16:14:55 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+//mysql database address
+define('DB_HOST','localhost');
+//mysql database user
+define('DB_USER','e_eeee1_cn');
+//database password
+define('DB_PASSWD','Shenle999.');
+//database name
+define('DB_NAME','e_eeee1_cn');
+//database prefix
+define('DB_PREFIX','eemlog_');
+//auth key
+define('AUTH_KEY','1vex(7W1WpDt%pK8i()HqEhOzTrbF0h$26a83cf185c4cbbc281de99ff84690e6');
+//cookie name
+define('AUTH_COOKIE_NAME','EM_AUTHCOOKIE_qPZ3gQon3cTvmAEFnnVu79vsGeBv5y8R');
